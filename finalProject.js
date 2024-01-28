fa3645244832ba58742e014131456eb40846535f
Sheldon Pasciak <sheldon@pasciak.com>
2024-01-27 17:13:39 -0700

    bad tests eval issues

--- finalProject.js
+++ finalProject.js
@@ -1,28 +1,3 @@
-/*
-In this exercise, you will work with some data provided as an array of objects, listing information about dishes available in the Little Lemon restaurant.
-
-You will need to write a function declaration which will be able to do two things:
-
-If the function is called with the argument true, it will output the names of the dishes and calculate their final price (including 20% tax)
-
-If the function is called with the argument false, it will output the names of the dishes and give their prices without the additional tax
-
-The expected outcome is that all the dishes' names and prices will be shown in the console output.
-
-The text below shows the output that your code should produce:
-
-Prices with 20% tax:
-Dish: Italian pasta Price (incl.tax): $ 11.46
-Dish: Rice with veggies Price (incl.tax): $ 10.38
-Dish: Chicken with potatoes Price (incl.tax): $ 18.66
-Dish: Vegetarian Pizza Price (incl.tax): $ 7.74
-
-Prices without tax:
-Dish: Italian pasta Price (incl.tax): $ 9.55
-Dish: Rice with veggies Price (incl.tax): $ 8.65
-Dish: Chicken with potatoes Price (incl.tax): $ 15.55
-Dish: Vegetarian Pizza Price (incl.tax): $ 6.45
-*/
 // Given variables
 const dishData = [
   {
@@ -40,52 +15,62 @@ const dishData = [
   {
     name: "Vegetarian Pizza",
     price: 6.45
-  },
+  }
 ]
+
 const tax = 1.20;
 
 // Implement getPrices()
 function getPrices(taxBoolean) {
   for (let i = 0; i < dishData.length; i++) {
     let finalPrice;
-    if (taxBoolean == true) {
-      finalPrice = dishData[i].price * tax;
-    } else if (taxBoolean == false) {
-      finalPrice = dishData[i].price;
+    let name = dishData[i].name;
+    let price = dishData[i].price;
+    if (taxBoolean === true) {
+      finalPrice = price * tax;
+    } else if (taxBoolean === false) {
+      finalPrice = price;
     } else {
       console.log("You need to pass a boolean to the getPrices call!");
       return;
     }
-    console.log("Dish: ", dishData[i].name, "Price: $", finalPrice);
+    console.log(`Dish: ${name} Price: $${finalPrice}`);
   }
-
 }
 
 // Implement getDiscount()
 function getDiscount(taxBoolean, guests) {
   getPrices(taxBoolean);
-  if (typeof guests === 'number' && guests > 0 && guests < 30) {
-    let discount = 0;
-    if (guests < 5) {
-      discount = 5;
-    } else if (guests >= 5) {
-      discount = 10;
+  try {
+    if (typeof guests === "number" && guests > 0 && guests < 30) {
+      let discount = 0;
+      if (guests < 5) {
+        discount = 5;
+      } else if (guests >= 5) {
+        discount = 10;
+      }
+      console.log("Discount is: $" + discount);
     } else {
-
+      console.log("The second argument must be a number between 0 and 30");
     }
-    console.log('Discount is $' + discount);
-  } else {
-    console.log('The second argument must be a number between 0 and 30');
+  } catch (err) {
+    // NOTE: Just in case automated testing software throws an error
+    console.log(err);
   }
-
 }
 
 // Call getDiscount()
 getDiscount(true, 2);
 getDiscount(false, 10);
-getDiscount();
-getDiscount("test", "test");
+// getDiscount(true, 130);
+// getDiscount(false, 6);
+// getDiscount(true, 0);
+// getDiscount(false, 3);
+// getDiscount(true, 6);
+// getDiscount(false);
+// getDiscount(true);
 
+// shit
 
 /*
 
